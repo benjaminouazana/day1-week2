ff0cca34fcf7f6570471e9f808ac121aee00df3d
ouazben <57089745+ouazben@users.noreply.github.com>
2019-11-03 22:04:41 +0200

    Add files via upload

---
+++ work1.js
@@ -0,0 +1,24 @@
+
+
+function ResultatAlerte(myhomework, work2){
+
+		//console.log('Im Benjamin ' + myhomework);
+		alert(myhomework);
+		var name = 'Hello Ben';
+		var age = 26;
+		alert('bonjour' + name + 'age' + age);
+	}
+
+function calcul(a, b) {
+	var resultat = a * b;
+	alert('resultat' + resultat);
+}
+
+ function conditionName(name) {
+ 	if (name == 'Ben') {
+ 		alert('Hello Ben');
+ 		} 
+ 	else {
+ 		alert ('Bye Ben');
+ 	    }
+ 	} 
